06975376af8edb0278f7754ca56e47ea0a157ae1
OrzPen <529234641@qq.com>
2019-07-27 19:27:55 +0800

    vant安装与rem适配

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-    app
+    <p class="test"></p>
     <van-button type="primary">默认按钮</van-button>
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
-
+<style lang="less" scoped>
+    .test{
+      width: 700px;
+      height: 100px;
+      background-color: skyblue;
+    }
 </style>
