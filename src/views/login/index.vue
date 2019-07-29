0255c37606001ff50ad0ab6f434534a018101ba4
OrzPen <529234641@qq.com>
2019-07-29 16:11:33 +0800

    登录-发送请求

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,20 +6,31 @@
         <van-field v-model="user.mobile" required clearable label="手机号" placeholder="请输入手机号" />
         <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码" required />
       </van-cell-group>
-      <van-button type="info" block>登录</van-button>
+      <van-button type="info" block @click.prevent="handleLogin">登录</van-button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'LoginIndex',
   data () {
     return {
       user: {
-        mobile: '',
-        code: ''
+        mobile: '18801185985',
+        code: '246810'
       }
+    }
+  },
+  methods: {
+    async handleLogin () {
+      const data = await axios({
+        method: 'post',
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        data: this.user
+      })
+      console.log(data)
     }
   }
 }
