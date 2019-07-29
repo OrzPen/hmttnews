fa220b8523382fd5543d22dd190e67a8dfc503c1
OrzPen <529234641@qq.com>
2019-07-29 18:00:52 +0800

    登录加载提示完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,7 +6,7 @@
         <van-field v-validate="'required|mobile'" name="phone" :error-message="errors.first('phone')" v-model="user.mobile" required clearable label="手机号" placeholder="请输入手机号" />
         <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码" required />
       </van-cell-group>
-      <van-button type="info" block @click.prevent="handleLogin">登录</van-button>
+      <van-button :loading='loginloading' type="info" block @click.prevent="handleLogin">登录</van-button>
     </form>
   </div>
 </template>
@@ -20,19 +20,24 @@ export default {
       user: {
         mobile: '18801185985',
         code: '246810'
-      }
+      },
+      loginloading: false
     }
   },
   methods: {
     handleLogin () {
+      this.loginloading = true
       try {
         this.$validator.validate().then(async valid => {
           if (!valid) {
+            this.loginloading = false
             return
           }
           const data = await login(this.user)
           console.log(data)
           this.$store.commit('setUser', data)
+          this.loginloading = false
+          this.$router.push('/')
         })
       } catch (error) {
         console.log(error)
