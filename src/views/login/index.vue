c7f7b8466009870e6feed9b9b77eed1ae44b5271
OrzPen <529234641@qq.com>
2019-07-29 16:02:31 +0800

    登录页面基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,29 @@
 <template>
-    <div>login</div>
+  <div>
+    <van-nav-bar title="登录" />
+    <form action="/" >
+      <van-cell-group>
+        <van-field v-model="user.mobile" required clearable label="手机号" placeholder="请输入手机号" />
+        <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码" required />
+      </van-cell-group>
+      <van-button type="info" block>登录</van-button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
 </style>
