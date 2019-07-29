47288d0c5a0260720b2f2ec5f422ae8323123217
OrzPen <529234641@qq.com>
2019-07-29 17:11:47 +0800

    表单验证-vee-validate配置

--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ import Vant from 'vant'
 import 'vant/lib/index.css'
 import 'amfe-flexible/index.min.js'
 import './styles/index.less'
+import VeeValidate, { Validator } from 'vee-validate'
+import CN from 'vee-validate/dist/locale/zh_CN'
+Vue.use(VeeValidate)
+
+Validator.localize('zh_CN', CN)
+
+// 自定义规则
+Validator.extend('mobile', {
+  getMessage: field => '请输入正确的手机号码',
+  validate: value =>
+    value.length === 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
+})
 Vue.use(Vant)
 Vue.config.productionTip = false
 
