d25fa2987988fb1b7eb18eefb88cc6bbb5447c1b
OrzPen <529234641@qq.com>
2019-07-29 17:22:06 +0800

    登录表单验证完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <van-nav-bar title="登录" />
     <form action="/" >
       <van-cell-group>
-        <van-field v-model="user.mobile" required clearable label="手机号" placeholder="请输入手机号" />
+        <van-field v-validate="'required|mobile'" name="phone" :error-message="errors.first('phone')" v-model="user.mobile" required clearable label="手机号" placeholder="请输入手机号" />
         <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码" required />
       </van-cell-group>
       <van-button type="info" block @click.prevent="handleLogin">登录</van-button>
@@ -24,11 +24,16 @@ export default {
     }
   },
   methods: {
-    async handleLogin () {
+    handleLogin () {
       try {
-        const data = await login(this.user)
-        console.log(data)
-        this.$store.commit('setUser', data)
+        this.$validator.validate().then(async valid => {
+          if (!valid) {
+            return
+          }
+          const data = await login(this.user)
+          console.log(data)
+          this.$store.commit('setUser', data)
+        })
       } catch (error) {
         console.log(error)
       }
