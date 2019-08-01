5edda3995653c2c074f37b201f87c70625b56eeb
OrzPen <529234641@qq.com>
2019-08-01 15:27:02 +0800

    文章列表-路由缓存

--- src/views/tabbar-layout/index.vue
+++ src/views/tabbar-layout/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <router-view></router-view>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
     <van-tabbar active-color="#0096fa" inactive-color="#000" v-model="activeIndex" route>
       <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
       <van-tabbar-item icon="search" to="/mine">我的</van-tabbar-item>
