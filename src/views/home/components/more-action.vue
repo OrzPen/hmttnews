70a7ac76dd1b05ce72b76aa339d180cf0f4700d4
OrzPen <529234641@qq.com>
2019-08-01 17:08:53 +0800

    更多操作基本布局

--- src/views/home/components/more-action.vue
+++ src/views/home/components/more-action.vue
@@ -1,6 +1,16 @@
 <template>
 <van-dialog closeOnClickOverlay :value="value" @input="$emit('input')" :showConfirmButton="false">
-    我是moreaction的内容
+     <van-cell-group v-if="!isReport">
+      <van-cell icon="location-o" title="不感兴趣" @click="handleUnlikeArticle()" />
+      <van-cell icon="location-o" title="反馈垃圾内容" is-link @click="isReport=true" />
+      <van-cell icon="location-o" title="拉黑作者" />
+    </van-cell-group>
+
+    <van-cell-group v-else>
+      <!-- 左按钮控制返回上一组结构 -->
+      <van-cell icon="arrow-left" @click="isReport=false" />
+      <van-cell v-for="(item,index) in reportType" :key="item.value" :title="item.title" icon="location-o" @click="handleClickReport(item,index)" />
+    </van-cell-group>
 </van-dialog>
 </template>
 
@@ -11,6 +21,25 @@ export default {
     value: {
       type: Boolean,
       default: false
+    }
+  },
+  data () {
+    return {
+      // 控制举报列表显示隐藏
+      isReport: false,
+      // 举报列表数据
+      reportType: [
+        // {title:'',value:0},
+        { title: '其他问题', value: 0 },
+        { title: '标题夸张', value: 1 },
+        { title: '低俗色情', value: 2 },
+        { title: '错别字多', value: 3 },
+        { title: '旧闻重复', value: 4 },
+        { title: '广告软文', value: 5 },
+        { title: '内容不实', value: 6 },
+        { title: '涉嫌违法犯罪', value: 7 },
+        { title: '侵权', value: 8 }
+      ]
     }
   }
 }
