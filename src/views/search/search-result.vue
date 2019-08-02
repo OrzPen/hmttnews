7901d3b2f181507327c1cf9a4146e6d920c766fc
OrzPen <529234641@qq.com>
2019-08-02 17:49:22 +0800

    搜索结果基本布局

--- src/views/search/search-result.vue
+++ src/views/search/search-result.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        {{$route.params.queryText}}
-    </div>
+  <div>
+    <van-nav-bar fixed title="搜索结果" left-text="返回" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <van-list
+      class="search-list"
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell v-for="item in list" :key="item" :title="item" />
+    </van-list>
+  </div>
 </template>
 
 <script>
@@ -12,7 +21,10 @@ export default {
     return {
       searchData: [],
       page: 1,
-      per_page: 10
+      per_page: 10,
+      list: [],
+      loading: false,
+      finished: false
     }
   },
   created () {
@@ -25,6 +37,21 @@ export default {
     }
   },
   methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    },
     async loadSearch () {
       const data = await getSearch({
         page: this.page,
@@ -37,9 +64,10 @@ export default {
     }
   }
 }
-
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.search-list {
+  margin-top: 92px;
+}
 </style>
