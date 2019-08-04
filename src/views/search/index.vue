fecfc07df6c5d9ef48c91ccaa474ea53a22a8fa5
OrzPen <529234641@qq.com>
2019-08-04 16:46:03 +0800

    搜索-历史记录功能布局完成

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -2,15 +2,33 @@
 <div>
   <van-search v-model="searchText" placeholder="请输入搜索关键词" show-action @search="onSearch(searchText)" @cancel="onCancel" />
   <!-- 联想建议 -->
-  <van-cell-group>
+  <van-cell-group v-if="!isDeleteData">
     <!-- 给搜索框添加搜索框同一个同一个事件 -->
     <van-cell v-for="(item, index) in suggestData" :key="index" icon="search" @click="onSearch(item)" >
         <span slot="title" v-html="highLight(item, searchText)"></span>
     </van-cell>
   </van-cell-group>
+  <van-cell-group v-if="suggestData.length&&searchText.length">
+      <van-cell icon="search" v-for="(item,i) in suggestData" :key="i" @click="onSearch(item)">
+        <div slot="title" v-html="highLight(item,searchText)">
+        </div>
+      </van-cell>
+    </van-cell-group>
+    <!-- 历史搜索记录 -->
+    <van-cell-group v-else>
+      <van-cell title="历史记录">
+        <van-icon @click="isDeleteData=true" v-show="!isDeleteData" slot="right-icon" name="delete" style="line-height:inherit"></van-icon>
+        <div v-show="isDeleteData">
+          <span style="margin-right:10px">全部删除</span>
+          <span @click="isDeleteData=false">完成</span>
+        </div>
+      </van-cell>
+      <van-cell title="hello111">
+        <van-icon v-show="isDeleteData" slot="right-icon" name="close" style="line-height:inherit"></van-icon>
+      </van-cell>
+    </van-cell-group>
 </div>
 
-<!-- 历史搜索记录 -->
 </template>
 
 <script>
@@ -23,7 +41,9 @@ export default {
       // 输入的关键字
       searchText: '',
       // 联想建议数据
-      suggestData: []
+      suggestData: [],
+      // 控制联想建议与历史记录的切换显示的数据
+      isDeleteData: false
     }
   },
   watch: {
