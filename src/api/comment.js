2d7f11e8c17ef332816b9a0836ace354fa32f4de
OrzPen <529234641@qq.com>
2019-08-06 11:23:26 +0800

    封装发布评论 | 回复api

--- src/api/comment.js
+++ src/api/comment.js
@@ -19,3 +19,21 @@ export const getComments = ({ source, offset = null, isComment = true }) => {
     }
   })
 }
+/**
+ * 添加评论或评论回复
+ * target integer 必须 评论的目标id（评论文章即为文章id，对评论进行回复则为评论id）
+ * content string 必须 评论内容
+ * art_id integer 非必须 文章id，对评论内容发表回复时，需要传递此参数，表明所属文章id。对文章进行评论，不要传此参数。
+ **/
+
+export const addComment = ({ target, content = '', artid = null }) => {
+  return $ajax({
+    method: 'POST',
+    url: `/app/v1_0/comments`,
+    data: {
+      target, // 评论->文章id  回复->评论id
+      content,
+      art_id: artid // 评论->不需要传   回复->需要传文章id
+    }
+  })
+}
