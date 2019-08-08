a90e56e6343775044c1b39b698bbce79b158f4f4
OrzPen <529234641@qq.com>
2019-08-08 20:16:23 +0800

    准备头像上传组件

--- src/views/mine/components/user-profile.vue
+++ src/views/mine/components/user-profile.vue
@@ -3,7 +3,7 @@
     <van-nav-bar title="个人信息" left-text="返回" right-text="保存" left-arrow @click-left="$router.back()" @click-right="handleSave" />
 
     <van-cell-group>
-      <van-cell is-link title="头像">
+      <van-cell is-link title="头像" @click="isUploadPhotoShow=true">
         <div slot="default">
           <img width="30" :src="user.photo" alt="">
         </div>
@@ -16,19 +16,22 @@
       <van-cell is-link title="性别" :value="user.gender === 0 ? '男' : '女'" />
       <van-cell is-link title="生日" :value="user.birthday" />
     </van-cell-group>
-
+    <upload-photo v-model="isUploadPhotoShow"></upload-photo>
   </div>
 </template>
 
 <script>
 import { getCurrentUserProfile, updateUserProfile } from '@/api/user'
+import UploadPhoto from './upload-photo.vue'
 export default {
   name: 'UserProfile',
   components: {
+    UploadPhoto
   },
   data () {
     return {
       user: {},
+      // 控制头像上传组件显示隐藏
       isUploadPhotoShow: false
     }
   },
